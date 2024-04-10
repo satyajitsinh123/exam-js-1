a67234d136d8de555c152b4defc7457334bf45d1
satyajitsinh123 <satyajitsinh63@gamil.com>
2024-04-10 11:09:57 +0530

    exam-js

---
+++ index.js
@@ -0,0 +1,24 @@
+// que---------3----------
+
+ 
+// let c = +prompt("celsius")
+// f=(c*1.8)+32
+// console.log(f)
+
+// que----2------
+
+let bassalary=+prompt("base salary")
+
+let hra=+prompt("hra")
+let da= +prompt("da")
+let ta= +prompt("ta")
+let base=bassalary+hra+da+ta/100
+console.log(base);
+
+// que---3-----
+
+// let first =+prompt("first angle")
+// let sec =+prompt("second angle")
+// let thrid =180-(first+sec)
+// console.log(thrid);
+
